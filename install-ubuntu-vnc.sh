76b0b4486b7267622ebd66e5016fb35a557ae2dc
Linkus <31256742+HUSKI3@users.noreply.github.com>
2020-02-28 20:29:36 +0100

    Update install-ubuntu-vnc.sh

--- install-ubuntu-vnc.sh
+++ install-ubuntu-vnc.sh
@@ -1,4 +1,5 @@
 apt update -y
+apt upgrade
 apt install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core -y
 apt install vnc4server -y 
 cp /usr/bin/vncserver /usr/bin/vncserver.bkp
