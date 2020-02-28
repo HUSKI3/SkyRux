0d2c79b3145cab5527ceb07fee58733d1c5deecb
Linkus <31256742+HUSKI3@users.noreply.github.com>
2020-02-28 20:26:19 +0100

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -6,9 +6,4 @@ git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh
-./start-ubuntu.sh
-sudo apt-get update -y
-sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core -y
-sudo apt-get install vnc4server -y 
-sudo cp /usr/bin/vncserver /usr/bin/vncserver.bkp
-vncserver
+
