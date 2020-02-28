075237807b68b1a956b70a3bd4e8aa73a28a468c
Linkus <31256742+HUSKI3@users.noreply.github.com>
2020-02-28 19:36:24 +0100

    Create install-ubuntu.sh

---
+++ install-ubuntu.sh
@@ -0,0 +1,14 @@
+pkg update && pkg upgrade
+pkg install git -y
+pkg install wget -y
+pkg install proot -y
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+cd termux-ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+./start-ubuntu.sh
+sudo apt-get update -y
+sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal gnome-core -y
+sudo apt-get install vnc4server -y 
+sudo cp /usr/bin/vncserver /usr/bin/vncserver.bkp
+vncserver
